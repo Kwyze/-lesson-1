d1c027cc4ef72eac150e7dd59478819bef3947e8
Dmitriy <>
2023-01-29 15:21:57 +0300

    first commit

---
+++ homework 3/Program.cs
@@ -0,0 +1,25 @@
+﻿ // Console.WriteLine("Введите первое число: ");
+        // Boolean a = Convert.ToBoolean(Console.ReadLine());
+        // Console.WriteLine("Введите второе число: ");
+        // Boolean b = Convert.ToBoolean(Console.ReadLine());
+        // Console.WriteLine("Введите третье число: ");
+        // Boolean c = Convert.ToBoolean(Console.ReadLine());
+        // if ((a > b > c)==0)
+        // {
+        //     Console.WriteLine(" " + a + "  " + b + " " + c);
+        // }
+
+        // if (a < b < c)
+        // {
+        //     Console.WriteLine(" " + c + "  " + b + " " + a);
+        // }
+
+        // if (a < b > c)
+        // {
+        //     Console.WriteLine(" " + b + "  " + c + " " + a);
+        // }
+
+        // if (a > b < c)
+        // {
+        //     Console.WriteLine(" " + c + "  " + a + " " + b);
+        // }
---
+++ homework 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a % 2)==0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
+
+ 
+
+
+for(int i=2; i<=8; i++) 
+{
+if(i%2 == 0) 
+Console.Write("{0} ", i);
+}
